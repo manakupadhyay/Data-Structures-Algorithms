634e7f26aa06814c393633dda84491817a5993e5
Manak Upadhyay <manu742000@gmail.com>
2020-06-07 10:50:16 +0530

    added Heap Sort

--- Sorting & Searching/HeapSort.cpp
+++ Sorting & Searching/HeapSort.cpp
@@ -1,36 +1,50 @@
 #include <iostream>
 using namespace std;
-void heapify(int arr[],int index){
+// maxHeap
+void heapify(int arr[],int index, int size)
+{
     // from top to bottom...(precolateDown)
     int left,right,min,temp;
     left = 2*index+1;
     right = 2*index+2;
-    if(left!=-1 && (arr[index] > arr[left]))
+    if(left< size && (arr[index] < arr[left]))
         min = left;
     else
         min = index;
-    if(right!=-1 && (arr[right] < arr[min]))
+    if(right<size && (arr[right] > arr[min]))
         min = right;
 
     if(min!=index){
         temp = arr[min];
         arr[min] = arr[index];
         arr[index] = temp;
-        heapify(arr,min);
+        heapify(arr,min,size);
     }
 }
 int main()
 {
-    int arr[]={65,78,11,23,1,66,73,120};
+    int arr[]={7,4,9,0,1,3,11,2};
     int size= sizeof(arr)/sizeof(arr[0]);
     cout << "Array is: ";
     for(int i=0;i<size;i++)
         cout << arr[i] << " ";
     cout << endl;
    
-        cout << "Array after sorting: ";
+    for(int i = size/2-1; i>=0; i--)
+        heapify(arr,i,size);
+    
+    for(int i = size-1;i>0;i--)
+    {
+        swap(arr[0],arr[i]);
+
+        heapify(arr,0,i);
+    }
+    
+    cout << "Array after sorting: ";
     for(int i=0;i<size;i++)
         cout << arr[i] << " ";
     cout << endl;
+
+
     return 0;
 }
